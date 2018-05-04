d80dfee7e7951a8aa53268fbd1d920ed5e986b01
Mudasar Rauf <mudassar.rauf@gmail.com>
2018-05-04 17:01:16 +0100

    First component

--- actions/user.js
+++ actions/user.js
@@ -1,4 +1,7 @@
-import { SET_USER_NAME, SET_USER_AVATAR, SET_USER_AUTHORIZING, SET_USER_AUTHORIZED  } from './actionTypes';
+import { SET_USER_NAME, SET_USER_AVATAR, SET_USER_AUTHORIZING, SET_USER_AUTHORIZED, USER_NO_EXIST, USER_LOGIN_FAILED } from './actionTypes';
+import firebase from '../firebase';
+
+import { fetchMessages } from './chatroom';
 
 export const setUserName = (name) => ({ type: SET_USER_NAME, name });
 export const setUserAvatar = (avatar) => 
@@ -7,6 +10,23 @@ export const setUserAvatar = (avatar) =>
         avatar: avatar && avatar.length > 0 ? avatar : 'https://abs.twimg.com/sticky/default_profile_images/default_profile_3_400x400.png' 
     });
     
-export const setUserAuthorizing = () => ({ type: SET_USER_AUTHORIZING });
-export const setUserAuthorized = () => ({ type: SET_USER_AUTHORIZED });
+export const startAuthorizing = () => ({ type: SET_USER_AUTHORIZING });
+export const userAuthorized = () => ({ type: SET_USER_AUTHORIZED });
+export const userLoginFailed = (error) => ({ type: USER_LOGIN_FAILED, error });
 
+export const userNoExist = () => ({
+    type: USER_NO_EXIST
+});
+
+export const login = ({email, password}) =>
+    (dispatch, getState) => {
+      dispatch(startAuthorizing());
+      firebase.auth().signInWithEmailAndPassword(email, password).then(auth => {
+          dispatch(userAuthorized());
+          dispatch(fetchMessages());
+      }).catch(
+          e => {
+              dispatch(userLoginFailed(e));
+          }
+      );
+    };
--- reducers/user.js
+++ reducers/user.js
@@ -1,10 +1,11 @@
-import { SET_USER_NAME, SET_USER_AVATAR, SET_USER_AUTHORIZING, SET_USER_AUTHORIZED  } from '../actions/actionTypes';
+import { SET_USER_NAME, SET_USER_AVATAR, SET_USER_AUTHORIZING, SET_USER_AUTHORIZED, USER_NO_EXIST, USER_LOGIN_FAILED } from '../actions/actionTypes';
 
 const userInitialState = {
     name: null,
     avatar: 'https://abs.twimg.com/sticky/default_profile_images/default_profile_3_400x400.png',
     authorizing: false,
-    authorized: false
+    authorized: false,
+    error: null
 };
 
 export const user = (state = userInitialState, action) => {
@@ -17,6 +18,13 @@ export const user = (state = userInitialState, action) => {
             return { ...state, authorizing: true };
         case SET_USER_AUTHORIZED:
             return { ...state, authorizing: false, authorized: true };
+        case USER_NO_EXIST:
+            return Object.assign({}, state, {
+                authorizing: false,
+                authorized: false
+            });
+        case USER_LOGIN_FAILED:
+            return {...state, authorizing: false, authorized: false };
         default:
             return state;
     }
